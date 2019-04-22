7a70a1f25016a2e2fba45a56d28cc45193b7a4ed
MARVIN\Phil <philldolan@gmail.com>
2019-04-22 15:59:17 +0100

    blender python stuff

--- Blender/1st curve import.py
+++ Blender/1st curve import.py
@@ -1,10 +1,11 @@
 import bpy
 import math
 import pdb
+import numpy as np
 from mathutils import Vector
 # to run in python console in blender
-# filename = this path
-# exec(compile(open(filename).read(), filename, 'exec'))
+# f0 = this path
+# exec(compile(open(filename).read(), f0, 'exec'))
 # print all objects
 for obj in bpy.data.objects:
     print(obj.name)
@@ -14,11 +15,21 @@ for obj in bpy.data.objects:
         bpy.data.objects.remove(obj)
 
 # for cur in bpy.data.curves:
-#     print(cur.name)
-#     bpy.data.curves.remove(cur)
 
+
+def sin_bcoords():
+    np.pi
+    x = np.linspace(0, 4 * np.pi, 20)
+    z = np.sin(x)
+    y = 0 * x
+    coords = []
+    for i0, j0 in enumerate(x):
+        c = [x[i0], y[i0], z[i0]]
+        coords.append(c)
+    return coords
+print(np.round(coords, 2))
 # sample data
-coords = [(1, 0, 1), (2, 0, 0), (3, 0, 1)]
+coords = sin_bcoords()
 
 # create the Curve Datablock
 curveData = bpy.data.curves.new('myCurve', type='CURVE')
@@ -26,7 +37,7 @@ curveData.dimensions = '3D'
 curveData.resolution_u = 2
 
 # map coords to spline
-polyline = curveData.splines.new('POLY')
+polyline = curveData.splines.new('NURBS')
 polyline.points.add(len(coords))
 for i, coord in enumerate(coords):
     x, y, z = coord
@@ -34,17 +45,17 @@ for i, coord in enumerate(coords):
 
 # create Object
 curveOB = bpy.data.objects.new('myCurve', curveData)
-curveData.bevel_depth = 0.01
+curveData.bevel_depth = 0.1
 bpy.ops.curve.primitive_bezier_circle_add(view_align=False,
                                           enter_editmode=False,
-                                          location=(
-                                              -1.09052,
-                                              -1.63116,
-                                              0.0890484))
-bpy.context.object.dimensions[0] = 2
-bpy.context.object.dimensions[1] = 2
+                                          location=(0, 0, 0))
+bpy.context.object.dimensions[0] = 0.05
+bpy.context.object.dimensions[1] = 0.05
+bpy.context.object.dimensions[2] = 0.05
 
-
+bpy.data.objects.data.objects[
+    'myCurve'].data.bevel_object = bpy.data.objects.data.objects[
+    'BezierCircle']
 # attach to scene and validate context
 scn = bpy.context.scene
 scn.objects.link(curveOB)
--- Blender/plot to coord.py
+++ Blender/plot to coord.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
-π = np.pi
-x = np.linspace(0, 4 * π, 100)
-z = np.sin(x)
-y = 0 * x
-coords = []
-for i0, j0 in enumerate(x):
-    c = [x[i0], y[i0], z[i0]]
-    coords.append(c)
-print(coords[0])
 
-plt.plot(x, z)
-plt.show()
+def sin_bcoords():
+    π = np.pi
+    x = np.linspace(0, 4 * π, 100)
+    z = np.sin(x)
+    y = 0 * x
+    coords = []
+    for i0, j0 in enumerate(x):
+        c = [x[i0], y[i0], z[i0]]
+        coords.append(c)
+    return coords
