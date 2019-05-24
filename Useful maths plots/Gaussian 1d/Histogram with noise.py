b4fb7a71cf23f2013a8df01e42cf644703c9fd67
Phil Dolan <philldolan@gmail.com>
2019-05-24 21:18:04 +0100

    bit of Gaussian histogram stuff going on

--- Useful maths plots/Gaussian 1d/Histogram with noise.py
+++ Useful maths plots/Gaussian 1d/Histogram with noise.py
@@ -2,52 +2,42 @@
 # Import some libraries
 ##############################################################################
 import sys
-import os
-import glob
-import time
-import re
 import numpy as np
-import scipy as sp
-import matplotlib
 import matplotlib.pyplot as plt
-import csv
-import scipy.optimize as opt
-import socket
-import scipy as sp
-import scipy.io as io
-import importlib.util
-import ntpath
-
-from scipy.interpolate import RectBivariateSpline
-from scipy.interpolate import interp1d
-from scipy.signal import find_peaks_cwt
-from scipy.ndimage.filters import gaussian_filter
-from mpl_toolkits.mplot3d import Axes3D
-from scipy.signal import savgol_filter
-from matplotlib import cm
 
 ##############################################################################
 # Import some extra special libraries from my own repo and do some other stuff
 ##############################################################################
-sys.path.insert(0, r"D:\Python\Local Repo\library")
+p_surface = r"C:\Users\Philip\Documents\GitHub\latest-python\library"
+sys.path.insert(0, p_surface)
 np.set_printoptions(suppress=True)
 import prd_plots
+import prd_maths
 import prd_file_import
 cs = prd_plots.palette()
 
 ##############################################################################
 # Do some stuff
 ##############################################################################
-p0 = (r"D:\Experimental Data\Internet Thorlabs optics data\PBS 102.txt")
-filepath = p0
-
+x = np.linspace(0.9, 1.1, 500)
+A = 50
+x_c = 1
+σ_x = 0.02
+bkg = 0
+SNR = 1 / 20
+bins = 15
+G = prd_maths.Gaussian_1D(x, A, x_c, σ_x, bkg)
+noise = np.random.normal(0, A * SNR, x.shape)
+G_noise = G + noise
+n, bins = np.histogram(noise, bins)
+Gauss_x, Gauss_y = prd_maths.Gauss_hist(n)
 ##############################################################################
 # Plot some figures
 ##############################################################################
 prd_plots.ggplot()
-plot_path = r"D:\Python\Plots\\"
-λ, cts = prd_file_import.load_spec(p0)
-print(cts[2, :])
+# plot_path = r"D:\Python\Plots\\"
+plot_path = r"C:\Users\Phil\Documents\GitHub\plots"
+plot_path = r"C:\Users\Philip\Documents\GitHub\plots"
 ###### image plot ############################################################
 # fig1 = plt.figure('fig1', figsize=(5, 5))
 # ax1 = fig1.add_subplot(1, 1, 1)
@@ -57,14 +47,23 @@ print(cts[2, :])
 # plt.imshow(im, extent=prd.extents(x) + prd.extents(y))
 
 ###### xy plot ###############################################################
-# size = 4
-# fig2 = plt.figure('fig2', figsize=(size * np.sqrt(2), size))
-# ax2 = fig2.add_subplot(111)
-# fig2.patch.set_facecolor(cs['mnk_dgrey'])
-# ax2.set_xlabel('x axis')
-# ax2.set_ylabel('y axis')
-# plt.plot(x1, y1, '.', alpha=0.4, color=cs['gglred'], label='')
-# plt.plot(x1, y1, alpha=1, color=cs['ggdred'], lw=0.5, label='decay')
+size = 4
+
+fig2 = plt.figure('fig2', figsize=(size * np.sqrt(2), size))
+ax2 = fig2.add_subplot(111)
+fig2.patch.set_facecolor(cs['mnk_dgrey'])
+ax2.set_xlabel('x axis')
+ax2.set_ylabel('y axis')
+plt.plot(noise, alpha=0.4, color=cs['gglred'], label='')
+
+fig3 = plt.figure('fig3', figsize=(size * np.sqrt(2), size))
+ax3 = fig3.add_subplot(111)
+fig3.patch.set_facecolor(cs['mnk_dgrey'])
+ax3.set_xlabel('x axis')
+ax3.set_ylabel('y axis')
+plt.hist(noise)
+plt.plot(Gauss_x,Gauss_y, alpha=0.4, color=cs['gglblue'], label='')
+# plt.hist(G_noise, 10, alpha=1, color=cs['ggdred'], lw=0.5, label='decay')
 # plt.plot(x2, y2, '.', alpha=0.4, color=cs['gglblue'], label='')
 # plt.plot(x2, y2, alpha=1, color=cs['ggblue'], lw=0.5, label='excite')
 
@@ -85,6 +84,7 @@ print(cts[2, :])
 # ax3.w_yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 # ax3.w_zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 # set_zlim(min_value, max_value)
-# plt.show()
-# plot_file_name = plot_path + 'plot1.png'
-# prd_plots.PPT_save_3d(fig3, ax3, plot_file_name)
+
+plt.show()
+plot_file_name = plot_path + 'plot1.png'
+prd_plots.PPT_save_2d(fig2, ax2, plot_file_name)
