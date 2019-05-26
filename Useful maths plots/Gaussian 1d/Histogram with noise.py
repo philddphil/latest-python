58310a92809b28a2611c225515f6785c41d8e796
MARVIN\Phil <philldolan@gmail.com>
2019-05-26 16:04:23 +0100

    got the histogram plots working

--- Useful maths plots/Gaussian 1d/Histogram with noise.py
+++ Useful maths plots/Gaussian 1d/Histogram with noise.py
@@ -4,12 +4,15 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
+
 
 ##############################################################################
 # Import some extra special libraries from my own repo and do some other stuff
 ##############################################################################
 p_surface = r"C:\Users\Philip\Documents\GitHub\latest-python\library"
-sys.path.insert(0, p_surface)
+p_home = r"C:\Users\Phil\Documents\GitHub\latest-python\library"
+sys.path.insert(0, p_home)
 np.set_printoptions(suppress=True)
 import prd_plots
 import prd_maths
@@ -21,23 +24,18 @@ cs = prd_plots.palette()
 ##############################################################################
 x = np.linspace(0.9, 1.1, 500)
 A = 50
-x_c = 1
-σ_x = 0.02
-bkg = 0
 SNR = 1 / 20
 bins = 15
-G = prd_maths.Gaussian_1D(x, A, x_c, σ_x, bkg)
-noise = np.random.normal(0, A * SNR, x.shape)
-G_noise = G + noise
-n, bins = np.histogram(noise, bins)
-Gauss_x, Gauss_y = prd_maths.Gauss_hist(n)
+value = 10
+noise = np.random.normal(0, A * SNR, x.shape) + value
+Gauss_x, Gauss_y = prd_maths.Gauss_hist(noise, bins)
 ##############################################################################
 # Plot some figures
 ##############################################################################
 prd_plots.ggplot()
 # plot_path = r"D:\Python\Plots\\"
 plot_path = r"C:\Users\Phil\Documents\GitHub\plots"
-plot_path = r"C:\Users\Philip\Documents\GitHub\plots"
+# plot_path = r"C:\Users\Philip\Documents\GitHub\plots"
 ###### image plot ############################################################
 # fig1 = plt.figure('fig1', figsize=(5, 5))
 # ax1 = fig1.add_subplot(1, 1, 1)
@@ -50,19 +48,23 @@ plot_path = r"C:\Users\Philip\Documents\GitHub\plots"
 size = 4
 
 fig2 = plt.figure('fig2', figsize=(size * np.sqrt(2), size))
-ax2 = fig2.add_subplot(111)
+gs = gridspec.GridSpec(1, 2, width_ratios=[3, 1])
+ax2 = fig2.add_subplot(gs[0])
 fig2.patch.set_facecolor(cs['mnk_dgrey'])
-ax2.set_xlabel('x axis')
-ax2.set_ylabel('y axis')
-plt.plot(noise, alpha=0.4, color=cs['gglred'], label='')
+ax2.set_xlabel('measurement number')
+ax2.set_ylabel('value')
+plt.plot(noise, '.', alpha=1, color=cs['gglred'], label='')
+plt.plot(noise, alpha=0.2, color=cs['gglred'], label='')
+[ymin, ymax] = ax2.get_ylim()
 
-fig3 = plt.figure('fig3', figsize=(size * np.sqrt(2), size))
-ax3 = fig3.add_subplot(111)
-fig3.patch.set_facecolor(cs['mnk_dgrey'])
-ax3.set_xlabel('x axis')
-ax3.set_ylabel('y axis')
-plt.hist(noise)
-plt.plot(Gauss_x,Gauss_y, alpha=0.4, color=cs['gglblue'], label='')
+ax3 = fig2.add_subplot(gs[1])
+fig2.patch.set_facecolor(cs['mnk_dgrey'])
+ax3.set_xlabel('freq of occurances (#)')
+ax3.set_ylabel('value')
+plt.hist(noise, bins, edgecolor=cs['mnk_dgrey'], color=cs['ggred'], alpha=0.5,
+         orientation="horizontal")
+plt.plot(Gauss_y, Gauss_x, alpha=1, color=cs['gglblue'], label='')
+ax3.set_ylim(ymin, ymax)
 # plt.hist(G_noise, 10, alpha=1, color=cs['ggdred'], lw=0.5, label='decay')
 # plt.plot(x2, y2, '.', alpha=0.4, color=cs['gglblue'], label='')
 # plt.plot(x2, y2, alpha=1, color=cs['ggblue'], lw=0.5, label='excite')
@@ -79,7 +81,7 @@ plt.plot(Gauss_x,Gauss_y, alpha=0.4, color=cs['gglblue'], label='')
 
 # ax3.legend(loc='upper right', fancybox=True, framealpha=0.5)
 # # os.chdir(p0)
-# plt.tight_layout()
+plt.tight_layout()
 # ax3.w_xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 # ax3.w_yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 # ax3.w_zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
