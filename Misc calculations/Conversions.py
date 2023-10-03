966a5601b663de126524a76b6834fe0b383011d8
philddphil <philldolan@gmail.com>
2023-10-03 15:18:33 +0100

    multiple usage changes

---
+++ Misc calculations/Conversions.py
@@ -0,0 +1,19 @@
+# %% imports
+import numpy as np
+import scipy as sp
+from scipy import constants
+# %% defs
+print(constants.c)
+lambda_nm = 1030
+lambda_m = lambda_nm * 1e-9
+nu_hz = constants.lambda2nu(lambda_m)
+nu_Mhz = nu_hz * 1e-6
+
+pulse_t_ns = 100
+pulse_t_s = pulse_t_ns * 1e-9
+# single photon values
+E_j = constants.h * nu_hz
+PulseP_W = E_j/pulse_t_s
+PulseP_pW = PulseP_W * 1e12
+
+
--- Misc calculations/dB to lin code.py
+++ Misc calculations/dB to lin code.py
@@ -1,11 +1,11 @@
 # %%
 import numpy as np
 # %%
-dB = -0.5
+dB = -1
 lin_from_dB = 10**(dB / 10)
 
 Pin = 1
-Pout = 0.9
+Pout = 0.
 dB_from_lin = 10 * np.log10(Pout/Pin)
 
 print('Linear ratio from dB value:', lin_from_dB)
